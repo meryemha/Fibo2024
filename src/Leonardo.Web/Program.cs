60e7b7722588fb3eac56874c521767d38c2e21be
meryemha <meryemhamidialaoui@gmail.com>
2024-12-17 16:47:58 +0100

    Commit du 17/12/2024

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -6,7 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<FibonacciDataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<Fibonacci>();
 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.MapGet("/", () => "Hello World 2!");
 
 app.MapGet("/fibonacci", async (Fibonacci fibonacci, ILogger<Program> logger) =>
